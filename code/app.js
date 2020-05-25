6935221bd781e501f92ea731fad30c1656073497
dibaosong <dibaosong@300.cn>
2020-05-25 13:49:51 +0800

    提交作业

--- code/app.js
+++ code/app.js
@@ -36,3 +36,57 @@ if(true){
 //3、
 var arr = [12, 34, 32, 89, 4];
 //console.log(Math.min(...arr));
+
+//5、
+var a = 10;
+var obj = {
+    a: 20,
+    fn(){
+        setTimeout(() => {
+            //console.log(this.a)
+        })
+    }
+}
+obj.fn();
+
+//8、
+// setTimeout(() => {
+//     console.log('3')
+// }, 0)
+// Promise.resolve(2).then((res) => {
+//     console.log(res)
+// })
+// console.log(1);
+
+//9、
+setTimeout(function(){
+    var a = "hello";
+    setTimeout(function(){
+        var b = "lagou";
+        setTimeout(function(){
+            var c = "I ♥ U";
+            //console.log(a + b + c)
+        }, 10)
+    }, 10)
+}, 10)
+
+function promiseLog (str) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function(){
+            resolve(str);
+        }, 10)
+    })
+}
+let str = '';
+promiseLog("hello").then((res) => {
+    str += res;
+    return promiseLog("lagou")
+}).then((res) => {
+    str += res;
+    return promiseLog("I ♥ U")
+}).then((res) => {
+    str += res;
+    console.log(str);
+})
+
+ 
